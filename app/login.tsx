284e58486ea5b2e5610574f5cae999b04218c835
whycodingsonangao <darrell.tan.2022@scis.smu.edu.sg>
2024-06-12 09:59:33 +0800

    deleted redundant code i added

--- app/login.tsx
+++ app/login.tsx
@@ -34,9 +34,6 @@ export default function Index() {
             setLoginError(errorCode);
             console.log('====================================');
         })
-        return{
-            style:{backgroundColor:"#0000"}
-        }
     }
     return (
         <View style={styles.container}>
